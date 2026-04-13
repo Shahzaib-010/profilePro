62358110f19b26fcb67e9c3789a33848049fb272
Shahzaib Anwar <letsmail.shahzaib@gmail.com>
2026-04-13 17:43:57 +0500

    Production Bug Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { getRouter } from "./router/router";
+
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { AuthProvider } from "./context/AuthContext";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { getRouter } from "./router/Router";
 
 const router = getRouter();
 
